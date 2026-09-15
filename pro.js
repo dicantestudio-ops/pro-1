536c77509d04f77f34eb32e8e63ccada01b4af13
DICANTE <Dicantestudio@gmail.com>
2026-09-15 11:06:00 +0100

    update header alignment

---
+++ pro.js
@@ -0,0 +1,11 @@
+let name = "total";
+let price = 50000;
+let quantity = 500;
+let age = 17;
+
+console.log(name);
+console.log(price);
+console.log(quantity);
+console.log(age);
+
+console.log(price*quantity);
